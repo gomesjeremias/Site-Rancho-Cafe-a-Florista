9a15665c3adb65a64aa47ecb570868f60607f32c
Jeremias Gomes da Silva <167638498+topo2024@users.noreply.github.com>
2025-06-30 10:35:05 -0300

    Configuração de responsividade

--- script.js
+++ script.js
@@ -19,3 +19,35 @@ window.addEventListener('click', e => {
     }
   });
 });
+
+const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+
+  // Fecha o menu ao clicar fora dele
+  document.addEventListener('click', (e) => {
+    const isClickInsideNav = navLinks.contains(e.target) || hamburger.contains(e.target);
+    if (!isClickInsideNav) {
+      navLinks.classList.remove('show');
+    }
+  });
+
+  const navLinksList = document.querySelectorAll('.nav-links li a');
+
+navLinksList.forEach(link => {
+  link.addEventListener('click', () => {
+    // Fecha o menu mobile se estiver aberto
+    if (navLinks.classList.contains('show')) {
+      navLinks.classList.remove('show');
+    }
+  });
+});
+
+  
+
+ 
+
+  
